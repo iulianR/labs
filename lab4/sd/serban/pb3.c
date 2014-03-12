bb2333b80c955fbb2924fc515a639a46e719d7a1
iulianR <iulian.gabriel.radu@gmail.com>
2014-03-13 01:35:39 +0200

    Added first 3 of 4-SD-Radu

---
+++ lab4/sd/serban/pb3.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nod {
+	int val;
+	struct nod* next;
+	struct nod* prev;
+} Nod, *Dlist;
+
+void adauga(Nod* &l, int x)
+{
+	Nod* aux, *nou = (Nod*) malloc (sizeof(Nod));
+
+	nou->val = x;
+	if (l == NULL || x <= l->val) {
+		nou->next = l;
+		l = nou;
+	} else {
+		aux = l;
+		while (aux->next != NULL && aux->next->val < x)
+			aux = aux->next;
+		nou->next = aux->next;
+		aux->next = nou;
+	}
+}
+
+void afisList (nod *l) 
+{
+    printf ("Elementele listei: ");
+    while (l != NULL) {
+        printf ("%d ", l->val);
+        l = l->next;
+    }
+    printf("\n");
+}
+
+void reverse (nod *&l)
+{
+	Nod* temp = NULL, *aux = l;
+	while (aux != NULL) {
+		temp = aux->next;
+		aux->next = aux->prev;
+		aux->prev = temp;
+		aux = aux->next;
+	}
+
+	l = temp->next;
+}
+
+int main()
+{
+	Nod* lst = NULL;
+	adauga(lst, 10);
+	adauga(lst, 15);
+	adauga(lst, 20);
+	adauga(lst, 30);
+	adauga(lst, 25);
+	adauga(lst, 35);
+	adauga(lst, 45);
+	adauga(lst, 13);
+	reverse(lst);
+	afisList(lst);
+	return 0;
+}
